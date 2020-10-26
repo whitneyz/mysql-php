c7ebf781a27edf33f6e3d4b8b06d35e09f722fd9
Whitneyz <66369610+whitneyz@users.noreply.github.com>
2020-10-26 11:39:47 +0100

    setup pdo connection database and also added checkboxes to README

---
+++ connection.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+function openDB()
+{
+    $dbhost = "localhost";
+    $dbuser = "becode";
+    $dbpass = "becode";
+    $db = "becode";
+
+    $driverOptions = [
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'",
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ];
+
+    return new PDO('mysql:host=' . $dbhost . ';dbname=' . $db, $dbuser, $dbpass, $driverOptions);
+}
+
